91d561682719cfdb00b2d1a62e9258bb3086c98b
RESizemore <44105841+RESizemore@users.noreply.github.com>
2019-01-10 16:50:06 -0600

    Updated with Backer generator
    Will read CSV and return Backer List

--- texture_painter.py
+++ texture_painter.py
@@ -1,4 +1,4 @@
-'''Renders text from a CSV file to textures
+"""Renders text from a CSV file to textures
 and applies them to multiple objectsself.
 
 Use Snippets...
@@ -7,7 +7,21 @@ import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import text
 
 ## To reload after a code changes
 import importlib; importlib.reload(texture_painter); texture_painter.go()
-'''
+"""
+
+import codecs
+import csv
+
+def get_backers(csv_filename):
+    with codecs.open (csv_filename, 'r', 'utf-8-sig') as stream:
+        iterable = csv.reader(stream)
+        header = next(iterable)
+        for row in iterable:
+            backer = dict(zip(header, row))
+            yield backer
 
 def go():
     print("Texture Painter starting up.")
+    # Read through the CSV
+    for backer in get_backers('backers_10.csv'):
+        print(backer)
