e95ea9ce728087f56a59da624db94a92300ff7b3
RESizemore <44105841+RESizemore@users.noreply.github.com>
2019-02-07 17:55:19 -0600

    Textures
    Texture outputs from Backer File

--- texture_painter.py
+++ texture_painter.py
@@ -9,8 +9,7 @@ import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import text
 import importlib; importlib.reload(texture_painter); texture_painter.go()
 """
 
-import codecs
-import csv
+import codecs, csv, os, bpy
 from PIL import Image, ImageFont, ImageDraw
 
 def get_backers(csv_filename):
@@ -22,15 +21,19 @@ def get_backers(csv_filename):
             yield backer
 
 #image size and font-size hard-coded in method
-def render_text_to_file(text_to_render):
-    image = Image.new('RGB', (128,128))
+def render_text_to_file(text_to_render, to_filename):
+    image = Image.new('RGB', (512,64))
     draw = ImageDraw.Draw(image)
     fnt = ImageFont.truetype('snake.ttf', 50)
     draw.text((0,0), text_to_render, font=fnt, fill=(255,255,255))
-    image.save("test.png")
+    image.save(to_filename)
 
 def go():
     print("Texture Painter starting up.")
+    cwd = os.path.dirname(bpy.data.filepath)
     # Read through the CSV
     for backer in get_backers('backers_10.csv'):
-        render_text_to_file("It worked!")
+        text_to_render = backer['Name'] + ', ' + backer['Country']
+        filename = cwd + '\\texture_cache\\' + backer['Number'] + '.png'
+        print("Rendering", text_to_render, "to", filename)
+        render_text_to_file(text_to_render, filename)
